f563a2da7be21edb1637fae8f33d864818405de1
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-09 16:17:20 +0100

    add fix controllo num inserito

--- js/main.js
+++ js/main.js
@@ -29,8 +29,6 @@ function randomInteger(min,max){
     return Math.floor(Math.random()* (max - min + 1)) +1 ;
 }
 
-
-
 /*--------------
     MAIN
 ----------------*/ 
@@ -38,51 +36,39 @@ function randomInteger(min,max){
 //con difficoltà 0 => tra 1 e 100
 //con difficoltà 1 => tra 1 e 80
 //con difficoltà 2 => tra 1 e 50
-
 let level = 0;
 let difficult = 0;
-
 do{
-    level = Number(prompt("scegli la difficoltà di gioco! 0 - bombe tra 1 e 100  1 - bombe tra 1 e 80  2 - bombe tra 1 e 50 "));
-
+    level = Number(prompt("scegli la difficoltà di gioco! 0 -tra 1 e 100  1 - tra 1 e 80  2 - tra 1 e 50 "));
     //determino il grado di difficoltà
     if (level === 0){
-        difficult = 100;
+        difficult = 20;
     } else if (level === 1){
-        difficult = 80;
+        difficult = 19;
     } else if (level === 2){
-        difficult = 50;
+        difficult = 18;
     }
-
 }while( level != 0 && level !=1 && level !=2 )
-
 console.log(difficult)
 //1.Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati
-
 let bombs = [];
 const nBombs = 16;
-
 while( bombs.length < nBombs){
     let bomb = randomInteger(1,100);
     if ( bombs.includes(bomb) === false ){
         bombs.push(bomb);
     } 
 }
-
 //stampo ARRAY bombs per verificare che i numeri siano tutti diversi tra loro
 console.log(bombs)
-
-// 2. In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-    //2a. L’utente non può inserire più volte lo stesso numero.
-    //2b. L’utente non può inserire più volte lo stesso numero.
-
 let i = 0 ;
 let selectNumbers = []; // utilizzo un array per prendere nota dei numeri già inseriti
-
 //condizione while per fare inserire all'utente N numeri meno il numero di bombe
 while(i < difficult - nBombs){
-    const num = Number(prompt("inserisci un numero"));
-
+    let num = 0;
+        do{
+            num = Number(prompt("inserisci un numero tra 1 e 100"));
+        }while(isNaN(num) || num < 1 || num > 100)
     //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 
     //controllo che il numero selezionato non sia presente nella lista delle bombe
@@ -90,7 +76,7 @@ while(i < difficult - nBombs){
         alert("mi dispiace hai beccato una bomba !!! hai perso - premere ok per riavviare");
         location.reload();
     }
-
+    // se il numero non è presente nella lista dei numeri inseriti lo pusha
     if(selectNumbers.includes(num) === false){
         selectNumbers.push(num);
         console.log(selectNumbers);
@@ -99,10 +85,8 @@ while(i < difficult - nBombs){
         alert("hai già inserito questo numero")
     }  
 }
-
 // contatore dei tentativi
 console.log("Numero di tentativi eseguiti =",i);
-
 // se il numero di tentativi corrisponde al numero di possibilità vuol dire che hai completato il gioco con successo
 if (i === difficult - nBombs){
     alert("Complimenti hai vinto !")

0d42dd8344d22bac0920bbc3c903f642a95fb2e7
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-09 14:41:31 +0100

    add array bombs

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 /*
 Il computer deve generare 16 numeri casuali tra 1 e 100.
 I numeri non possono essere duplicati.
+
 In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
 L’utente non può inserire più volte lo stesso numero.
 Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
@@ -12,4 +14,38 @@ con difficoltà 0 => tra 1 e 100
 con difficoltà 1 => tra 1 e 80
 con difficoltà 2 => tra 1 e 50
 */
+
+/*--------------
+    FUNCTIONS
+----------------*/ 
+
+// GENERA NUMERI RANDOMICI 
+function randomInteger(min,max){
+    return Math.floor(Math.random()* (max - min + 1)) +1 ;
+}
+
+
+
+/*--------------
+    MAIN
+----------------*/ 
+
+//1.Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati
+
+let bombs = [];
+const nBombs = 16;
+
+while( bombs.length < nBombs){
+    let bomb = randomInteger(1,100);
+    if ( bombs.includes(bomb) === false ){
+        bombs.push(bomb);
+    } 
+}
+//stampo ARRAY bombs per verificare che i numeri siano tutti diversi tra loro
+console.log(bombs)
+
+// 2. In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
+
+
+
 

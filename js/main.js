a43d0bd76ab85b4e621c0fb32aa1f35037126a6a
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-09 15:47:27 +0100

    add bonus

--- js/main.js
+++ js/main.js
@@ -34,7 +34,29 @@ function randomInteger(min,max){
 /*--------------
     MAIN
 ----------------*/ 
+// scelta difficoltà 
+//con difficoltà 0 => tra 1 e 100
+//con difficoltà 1 => tra 1 e 80
+//con difficoltà 2 => tra 1 e 50
 
+let level = 0;
+let difficult = 0;
+
+do{
+    level = Number(prompt("scegli la difficoltà di gioco! 0 - bombe tra 1 e 100  1 - bombe tra 1 e 80  2 - bombe tra 1 e 50 "));
+
+    //determino il grado di difficoltà
+    if (level === 0){
+        difficult = 20;
+    } else if (level === 1){
+        difficult = 19;
+    } else if (level === 2){
+        difficult = 18;
+    }
+
+}while( level != 0 && level !=1 && level !=2 )
+
+console.log(difficult)
 //1.Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati
 
 let bombs = [];
@@ -58,7 +80,7 @@ let i = 0 ;
 let selectNumbers = []; // utilizzo un array per prendere nota dei numeri già inseriti
 
 //condizione while per fare inserire all'utente N numeri meno il numero di bombe
-while(i < 20 - nBombs){
+while(i < difficult - nBombs){
     const num = Number(prompt("inserisci un numero"));
 
     //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
@@ -82,7 +104,7 @@ while(i < 20 - nBombs){
 console.log("Numero di tentativi eseguiti =",i);
 
 // se il numero di tentativi corrisponde al numero di possibilità vuol dire che hai completato il gioco con successo
-if (i === 20 - nBombs){
+if (i === difficult - nBombs){
     alert("Complimenti hai vinto !")
 }
 

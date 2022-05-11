bcddebbc15f2e6b60cd84f6172813fec765e49f4
GiuseppeTodisco92 <giuseppe24todisco92@gmail.com>
2022-05-11 20:27:13 +0100

    add fix and style

--- campo-minato-grafica/js/main.js
+++ campo-minato-grafica/js/main.js
@@ -1,97 +1,19 @@
-// /*
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// I numeri non possono essere duplicati.
-
-// In seguito deve chiedere all’utente (100 - 16) volte di inserire un numero alla volta, sempre compreso tra 1 e 100.
-
-// L’utente non può inserire più volte lo stesso numero.
-
-// Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-
-// La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
-
-// Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
-
-
-// BONUS: (da fare solo se funziona tutto il resto)
-// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
-// con difficoltà 0 => tra 1 e 100
-// con difficoltà 1 => tra 1 e 80
-// con difficoltà 2 => tra 1 e 50
-// */
-
-
-// /*--------------
-//     MAIN
-// ----------------*/ 
-// // scelta difficoltà 
-// //con difficoltà 0 => tra 1 e 100
-// //con difficoltà 1 => tra 1 e 80
-// //con difficoltà 2 => tra 1 e 50
-// let level = 0;
-// let difficult = 0;
-// /*do{
-//     level = Number(prompt("scegli la difficoltà di gioco! 0 -tra 1 e 100  1 - tra 1 e 80  2 - tra 1 e 50 "));
-//     determino il grado di difficoltà
-//     if (level === 0){
-//         difficult = 100;
-//     } else if (level === 1){
-//         difficult = 80;
-//     } else if (level === 2){
-//         difficult = 50;
-//     }
-// }while(isNaN(level) || level != 0 && level !=1 && level !=2 )
-// console.log(difficult)*/
-// //1.Il computer deve generare 16 numeri casuali tra 1 e 100. I numeri non possono essere duplicati
-
-// //stampo ARRAY bombs per verificare che i numeri siano tutti diversi tra loro
-// console.log(bombs)
-// let i = 0 ;
-// let selectNumbers = []; // utilizzo un array per prendere nota dei numeri già inseriti
-// //condizione while per fare inserire all'utente N numeri meno il numero di bombe
-// while(i < levelSelect){
-//     let num = 0;
-//         do{
-//             //num = Number(prompt("inserisci un numero tra 1 e 100"));
-//         }while(isNaN(num) || num < 1 || num > 100)
-//     //Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
-
-//     //controllo che il numero selezionato non sia presente nella lista delle bombe
-//     if (bombs.includes(num) === true){
-//         alert("mi dispiace hai beccato una bomba !!! hai perso - premere ok per riavviare");
-//         break;
-//     }
-//     // se il numero non è presente nella lista dei numeri inseriti lo pusha
-//     if(selectNumbers.includes(num) === false){
-//         selectNumbers.push(num);
-//         console.log(selectNumbers);
-//         i++; // continuo il ciclo 
-//     }else{
-//         alert("hai già inserito questo numero")
-//     }  
-// }
-// // contatore dei tentativi
-// console.log("Numero di tentativi eseguiti =",i);
-// // se il numero di tentativi corrisponde al numero di possibilità vuol dire che hai completato il gioco con successo
-// if (i === levelSelect){
-//     alert("Complimenti hai vinto !")
-// }
-
-
-
 
 /*--------------
     MAIN
 ----------------*/ 
-//!creare la selezione delle difficoltà
+
 const playBtn = document.querySelector(".btn_custom");
 const boxList = document.querySelector(".box_square");
 let bombs = [];
 const nBombs = 16;
 let difficult = 0; //da sistemare aggiugnere i livelli
 let possiblity = difficult - nBombs;
-// creazione bombe
+
+//invoca funzione livello 
 level();
+
+// creazione bombe
 while( bombs.length < nBombs){
     let bomb = randomInteger(1,100);
     if ( bombs.includes(bomb) === false ){
@@ -113,7 +35,7 @@ let number = [];
 for (let i=1 ; i <= difficult ; i++){
     let box = document.createElement("div");
     box.classList.add("square");
-    box.innerText = i;
+    // box.innerText = i;
     number.push(i);
     boxList.append(box);
     point.innerText = score;
@@ -121,7 +43,9 @@ for (let i=1 ; i <= difficult ; i++){
     box.addEventListener("click", function bomb(){
         if(number.includes(i) != bombs.includes(i)){
             box.style.backgroundColor = "green";
-             score += 100;
+            const flower = `<i class="fa-solid fa-seedling"></i>`
+            box.innerHTML = flower;
+            score += 100;
              point.innerText = score;
              if(score/100 === possiblity){
                  alert("hai vinto");
@@ -130,14 +54,14 @@ for (let i=1 ; i <= difficult ; i++){
         }
         else if(bombs.includes(i)){
             box.style.backgroundColor = "red";
+            const bombImage = `<i class="fa-solid fa-bomb"></i>`
+            box.innerHTML = bombImage;
             result.innerText = "Hai beccato una bomba ! play per riavviare!";
             alert("premere play per riavviare il gioco");
             refreshPage();
         }
       }
     )
-
-    
 }
 
 
@@ -165,4 +89,5 @@ function level(){
         difficult = 50; 
     }
 }
+
 
